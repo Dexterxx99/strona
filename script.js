79c33caefd3c52e7ffd2f042136590577984b6a2
Dexterxx99 <dextertiktok99@gmail.com>
2024-08-20 02:09:56 +0200

    script.js

--- script.js
+++ script.js
@@ -29,18 +29,7 @@ document.getElementById('video-options').addEventListener('change', function() {
             'season2': 'Sezon 2',
             'season3': 'Sezon 3'
         };
-    } else if (selectedValue === 'jezdzcy-smokow') {
-        seasons = {
-            'season1': 'Sezon 1',
-            'season2': 'Sezon 2',
-            'season3': 'Sezon 3',
-            'season4': 'Sezon 4',
-            'season5': 'Sezon 5',
-            'season6': 'Sezon 6',
-            'season7': 'Sezon 7',
-            'season8': 'Sezon 8'
-        };
-    }
+    } 
 
     populateSeasons(seasons);
 });
@@ -105,14 +94,7 @@ function getEpisodesCount(season) {
         'season1-generator-rex': 20,
         'season2-generator-rex': 20,
         'season3-generator-rex': 20,
-        'season1-jezdzcy-smokow': 20,
-        'season2-jezdzcy-smokow': 20,
-        'season3-jezdzcy-smokow': 13,
-        'season4-jezdzcy-smokow': 13,
-        'season5-jezdzcy-smokow': 13,
-        'season6-jezdzcy-smokow': 13,
-        'season7-jezdzcy-smokow': 13,
-        'season8-jezdzcy-smokow': 13,
+        
     };
 
     return episodesCount[season] || 10;
@@ -317,143 +299,7 @@ function getVideoUrl(series, season, episode) {
                 'odcinek-20': 'https://www.youtube.com/watch?v=example152'
             }
         },
-        'jezdzcy-smokow': {
-            'season1': {
-                'odcinek-1': 'https://drive.google.com/file/d/1PmKhGr0b0NCrLapA-kH1a_KOjEotA9EE/view',
-                'odcinek-2': 'https://drive.google.com/file/d/12bt923JUaCVa3BTbvEjSvfFeEz4jlV7f/view',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example14',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example15',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example16',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example17',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example18',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example19',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example20',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example21',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example22',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example23',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example24',
-                'odcinek-14': 'https://drive.google.com/drive/folders/example25',
-                'odcinek-15': 'https://drive.google.com/drive/folders/example26',
-                'odcinek-16': 'https://drive.google.com/drive/folders/example27',
-                'odcinek-17': 'https://drive.google.com/drive/folders/example28',
-                'odcinek-18': 'https://drive.google.com/drive/folders/example29',
-                'odcinek-19': 'https://drive.google.com/drive/folders/example30',
-                'odcinek-20': 'https://drive.google.com/drive/folders/example31',
-            },
-            'season2': {
-                'odcinek-1': 'https://drive.google.com/drive/folders/example32',
-                'odcinek-2': 'https://drive.google.com/drive/folders/example33',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example34',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example35',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example36',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example37',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example38',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example39',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example40',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example41',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example42',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example43',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example44',
-                'odcinek-14': 'https://drive.google.com/drive/folders/example45',
-                'odcinek-15': 'https://drive.google.com/drive/folders/example46',
-                'odcinek-16': 'https://drive.google.com/drive/folders/example47',
-                'odcinek-17': 'https://drive.google.com/drive/folders/example48',
-                'odcinek-18': 'https://drive.google.com/drive/folders/example49',
-                'odcinek-19': 'https://drive.google.com/drive/folders/example50',
-                'odcinek-20': 'https://drive.google.com/drive/folders/example51'
-            },
-            'season3': {
-                'odcinek-1': 'https://drive.google.com/drive/folders/example52',
-                'odcinek-2': 'https://drive.google.com/drive/folders/example53',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example54',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example55',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example56',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example57',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example58',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example59',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example60',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example61',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example62',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example63',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example64',
-            },
-            'season4': {
-                'odcinek-1': 'https://drive.google.com/drive/folders/example65',
-                'odcinek-2': 'https://drive.google.com/drive/folders/example66',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example67',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example68',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example69',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example70',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example71',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example72',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example73',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example74',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example75',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example76',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example77'
-                
-            },
-            'season5': {
-                'odcinek-1': 'https://drive.google.com/drive/folders/example78',
-                'odcinek-2': 'https://drive.google.com/drive/folders/example79',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example80',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example81',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example82',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example83',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example84',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example85',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example86',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example87',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example88',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example89',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example90'
-            },
-            'season6': {
-                'odcinek-1': 'https://drive.google.com/drive/folders/example91',
-                'odcinek-2': 'https://drive.google.com/drive/folders/example92',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example93',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example94',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example95',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example96',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example97',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example98',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example99',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example100',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example101',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example102',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example103'
-            },
-            'season7': {
-                'odcinek-1': 'https://drive.google.com/drive/folders/example104',
-                'odcinek-2': 'https://drive.google.com/drive/folders/example105',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example106',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example107',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example108',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example109',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example110',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example111',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example112',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example113',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example114',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example115',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example116'
-            },
-            'season8': {
-                'odcinek-1': 'https://drive.google.com/drive/folders/example117',
-                'odcinek-2': 'https://drive.google.com/drive/folders/example118',
-                'odcinek-3': 'https://drive.google.com/drive/folders/example119',
-                'odcinek-4': 'https://drive.google.com/drive/folders/example120',
-                'odcinek-5': 'https://drive.google.com/drive/folders/example121',
-                'odcinek-6': 'https://drive.google.com/drive/folders/example122',
-                'odcinek-7': 'https://drive.google.com/drive/folders/example123',
-                'odcinek-8': 'https://drive.google.com/drive/folders/example124',
-                'odcinek-9': 'https://drive.google.com/drive/folders/example125',
-                'odcinek-10': 'https://drive.google.com/drive/folders/example126',
-                'odcinek-11': 'https://drive.google.com/drive/folders/example127',
-                'odcinek-12': 'https://drive.google.com/drive/folders/example128',
-                'odcinek-13': 'https://drive.google.com/drive/folders/example129'
-            }
-        }
+        
     };
 
     return urls[series] && urls[series][season] && urls[series][season][episode] || '#';
